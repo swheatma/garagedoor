316e21eb504b9acf500cf67cd31aca5a04684fcd
Steven Wheatman <stwheatman@gmail.com>
2016-12-09 20:29:41 -0500

    It's working with this version

---
+++ python/garage.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as GPIO
+import time
+# GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)
+GPIO.setup(12, GPIO.IN)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+left = 11
+right = 12
+R_Light = 15
+L_Light = 22
+delay = 4
+T1 = 0
+T2 = 0
+TE = 0
+OPEN = "open"
+CLOSED = "closed"
+Rcounter = 0
+Lcounter = 0
+#check door status
+def door_status(x1):
+    return OPEN if GPIO.input(x1) else CLOSED
+
+def closeDoor(door):
+    GPIO.output(door, True)
+    time.sleep(1)
+    GPIO.output(door,False)
+
+GPIO.output(R_Light,False)
+
+while True:
+    if door_status(right) == OPEN:
+        if Rcounter > delay:
+            print "Right Door has been open too long, closing..."
+            closeDoor(R_Light)
+            Rcounter = 0
+        else:
+            Rcounter+=1
+            print "Right Door has been open ", Rcounter, " second"
+    else:
+        print "Right Door is closed"
+        Rcounter = 0
+    if door_status(left) == OPEN:
+        if Lcounter > delay:
+            print "Left Door has been open too long, closing..."
+            closeDoor(L_Light)
+            Lcounter = 0
+        else:
+            Lcounter+=1
+            print "Left Door has been open ", Lcounter, " second"
+    else:
+        print "Left Door is closed"
+        Lcounter = 0
+    time.sleep(1)
---
+++ python/script.py
@@ -0,0 +1,26 @@
+import webiopi
+#SW import datetime
+
+GPIO = webiopi.GPIO
+
+LIGHT = 22 # GPIO pin using BCM numbering
+LIGHT2 = 25 
+
+# setup function is automatically called at WebIOPi startup
+def setup():
+    # set the GPIO used by the light to output
+    GPIO.setFunction(LIGHT, GPIO.OUT)
+    GPIO.setFunction(LIGHT2, GPIO.OUT)
+
+# loop function is repeatedly called by WebIOPi 
+def loop():
+    # retrieve current datetime
+#SW    now = datetime.datetime.now()
+
+    # gives CPU some time before looping again
+    webiopi.sleep(1)
+
+# destroy function is called at WebIOPi shutdown
+def destroy():
+    GPIO.digitalWrite(LIGHT, GPIO.LOW)
+    GPIO.digitalWrite(LIGHT2, GPIO.LOW)
